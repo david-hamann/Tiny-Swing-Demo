4f7a9232bbeef878e4bc8eee181457aff32ac9f6
David Hamann <david.hamann@nike.com>
2024-01-08 13:32:57 -0800

    ad scroll pane

--- src/main/java/com/flamingmarshmallow/demo/gui/IndexPane.java
+++ src/main/java/com/flamingmarshmallow/demo/gui/IndexPane.java
@@ -3,13 +3,14 @@ package com.flamingmarshmallow.demo.gui;
 import java.awt.Color;
 import java.awt.GridLayout;
 import java.util.Map;
-import java.util.Map.Entry;
 
 import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 import com.flamingmarshmallow.demo.service.Widget;
 
+@SuppressWarnings("serial")
 public class IndexPane extends JPanel {
 	
 	public IndexPane(final JList<Map.Entry<Long, Widget>> objectList) {
@@ -18,7 +19,7 @@ public class IndexPane extends JPanel {
 		
 		objectList.setBackground(Color.CYAN);
 		objectList.setForeground(Color.BLACK);
-		add(objectList);
+		add(new JScrollPane(objectList));
 	}
 	
 	
